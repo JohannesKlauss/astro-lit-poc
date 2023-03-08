b11626dd13ec5dada1c9c57ad758a2d4a51558d4
Johannes Klauss <klauss.johannes@gmail.com>
2023-03-08 09:47:57 +0100

    remove node_modules

--- dist/server/chunks/prerender.6b28b7a7.mjs
+++
@@ -1,2 +0,0 @@
-const noop = () => {};
-export const _ = noop;
--- node_modules/@astrojs/lit/dist/client.d.ts
+++
@@ -1,5 +0,0 @@
-declare const _default: (element: HTMLElement) => (Component: any, props: Record<string, any>, { default: defaultChildren, ...slotted }: {
-    [slotName: string]: string;
-    default: string;
-}) => Promise<void>;
-export default _default;
