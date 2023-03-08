92b23d5a62671547af2bb8902954938b8564c300
Johannes Klauss <klauss.johannes@gmail.com>
2023-03-08 09:49:52 +0100

    add vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import lit from "@astrojs/lit";
-
 import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [lit()],
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel()
 });
